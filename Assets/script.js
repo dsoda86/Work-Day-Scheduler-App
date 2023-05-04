0031988b638faca7ebbd9fefd819a6c890349855
dsoda86 <122141740+dsoda86@users.noreply.github.com>
2023-05-03 22:38:12 -0700

    Used id of each time block to get saved input from local storage. Input stays after page refresh!

--- Assets/script.js
+++ Assets/script.js
@@ -33,6 +33,7 @@ $(document).ready(function () {
    function hourTracker () {
       //Gets current hour in 24-hour time.
     var currentHour = dayjs().hour() 
+    console.log(currentHour)
     $(".time-block").each(function() {
       // Uses parseInt and split on the block's id to get the number part of the string and store it in a variable.
       var blockTime = parseInt($(this).attr("id").split("-")[1]);
@@ -57,6 +58,16 @@ $(document).ready(function () {
   // attribute of each time-block be used to do this?
   //
   // need a function or method to retrieve and show saved input in textArea
+
+   $('#hour-09 .description').val(localStorage.getItem('hour-09'));
+   $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+   $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+   $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+   $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+   $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+   $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+   $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+   $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
   // TODO: Add code to display the current date in the header of the page.
   function displayTime() {
